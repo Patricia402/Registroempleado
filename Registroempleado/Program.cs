e24a923d28e89f6cf0d2ace21d2ae48e72193d57
Patricia <pimembreno@uamv.edu.ni>
2026-05-29 12:05:39 -0600

    Agregue la funcion agregarEmpleados

--- Registroempleado/Program.cs
+++ Registroempleado/Program.cs
@@ -1,1 +1,24 @@
 ﻿
+Empleado[] empleado = new Empleado[10];
+
+void agregarEmpleado(int pos)
+{
+    Console.Write("Nombres: ");
+    empleado[pos].nombres = Console.ReadLine();
+    Console.Write("Apellidos: ");
+    empleado[pos].apellidos = Console.ReadLine();
+    Console.Write("Cargo: ");
+    empleado[pos].cargo = Console.ReadLine();
+   Console.Write("Salario: ");
+    empleado[pos].salario = double.Parse(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Registro guardado satisfactoriamente");
+    Console.ResetColor();
+}
+    struct Empleado
+{
+   public string nombre;
+   public string apellido;
+   public string cargo;
+   public double salario;
+}
